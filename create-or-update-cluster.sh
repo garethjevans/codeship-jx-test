91b899d66f2bae576e131ebdb1ad9ec1d44e4b3b
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-07-10 11:31:45 +0100

    Added commands to create a jx cluster

--- create-or-update-cluster.sh
+++ create-or-update-cluster.sh
@@ -4,17 +4,24 @@ set -euo pipefail
 
 JX_VERSION=1.3.76
 
+ORG=myorg03
+SA=~/.gke_sa.json
+
 function install_dependencies() {
 	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
 	tar xvf jx-linux-amd64.tar.gz
 }
 
 function configure_environment() {
-#	echo $GKE_SA_JSON > ~/.gke_sa.json
+	echo ${GKE_SA_JSON} > ${SA}
+	mkdir -p ~/.jx/organisations/organisation-${ORG}
+    cp -R clusters ~/.jx/organisations/organisation-${ORG}
+
 }
 
 function apply() {
 	./jx version
+	./jx create terraform -c 'dev=gke' -b -o ${ORG} --gke-service-account ${SA}
 }
 
 install_dependencies
