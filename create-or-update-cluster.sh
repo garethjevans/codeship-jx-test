20859930427b77c99454d67a42ab4d3e7ed60361
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-07-10 11:15:03 +0100

    removed debugging around jx install

--- create-or-update-cluster.sh
+++ create-or-update-cluster.sh
@@ -3,17 +3,10 @@
 set -euo pipefail
 
 JX_VERSION=1.3.76
-TERRAFORM_VERSION=0.11.7
 
 function install_dependencies() {
-	wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-	unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-	rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
 	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
-	ls -al
 	tar xvf jx-linux-amd64.tar.gz
-	ls -al
 }
 
 #function configure_environment() {
