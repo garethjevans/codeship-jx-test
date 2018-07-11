84ec15ffe1dbc2d14e48b2e298ee6e532860e4b7
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-07-11 13:16:38 +0100

    Installing terraform as a prereq

--- create-or-update-cluster.sh
+++ create-or-update-cluster.sh
@@ -4,9 +4,18 @@ set -euo pipefail
 
 SA=~/.gke_sa.json
 
+TERRAFORM_VERSION=0.11.7
+
 function install_dependencies() {
 	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
 	tar xvf jx-linux-amd64.tar.gz
+
+	mkdir -p ~/.jx/bin
+	
+	wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+	unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+	rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+	mv terraform ~/.jx/bin
 }
 
 function configure_environment() {
