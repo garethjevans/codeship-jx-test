233f77959094ec51c6e67e7fcf9a8bb8c23775b3
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-07-12 14:21:05 +0100

    using --install-dependencies flag

--- create-or-update-cluster.sh
+++ create-or-update-cluster.sh
@@ -4,29 +4,9 @@ set -euo pipefail
 
 SA=~/.gke_sa.json
 
-TERRAFORM_VERSION=0.11.7
-KUBERNETES_VERSION=1.9.9
-HELM_VERSION=2.9.1
-
 function install_dependencies() {
 	wget https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
 	tar xvf jx-linux-amd64.tar.gz
-
-	mkdir -p ~/.jx/bin
-	
-	wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-	unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-	rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-	mv terraform ~/.jx/bin
-	
-	wget https://storage.googleapis.com/kubernetes-release/release/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl
-	chmod +x kubectl
-	mv kubectl ~/.jx/bin
-
-	wget https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz
-	tar xvf helm-v${HELM_VERSION}-linux-amd64.tar.gz
-	rm helm-v${HELM_VERSION}-linux-amd64.tar.gz
-	mv linux-amd64/helm ~/.jx/bin
 }
 
 function configure_environment() {
@@ -36,7 +16,7 @@ function configure_environment() {
 }
 
 function apply() {
-	./jx create terraform --verbose -c 'dev=gke' -b -o ${ORG} --gke-service-account ${SA}
+	./jx create terraform --verbose -c 'dev=gke' -b --install-dependencies -o ${ORG} --gke-service-account ${SA}
 }
 
 install_dependencies
